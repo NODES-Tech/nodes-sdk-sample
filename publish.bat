c0f8f100e3c7a9ceca3d19994ebd10ba74c93d1a
Narve Sætre <narve.saetre@ae.no>
2020-01-23 19:53:49 +0100

    update publish

--- publish.bat
+++ publish.bat
@@ -5,5 +5,5 @@ git merge master
 dotnet publish
 copy ConsoleApplication\bin\Debug\netcoreapp3.0\publish\*.* publish
 copy ConsoleApplication\bin\Debug\publish\*.* publish
-echo  git commit -a --all -m "publish"
-echo  git push
+git commit -a --all -m "publish"
+git push
