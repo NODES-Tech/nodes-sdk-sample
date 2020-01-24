d66832416d89ee207dc36fb5f18b36060cd2ad4a
Narve Sætre <narve.saetre@ae.no>
2020-01-24 09:38:06 +0100

    publish

--- publish.bat
+++ publish.bat
@@ -1,6 +1,6 @@
-git checkout master
-git pull
-git checkout binaries
+echo git checkout master
+echo git pull
+echo git checkout binaries
 git merge master
 dotnet publish
 copy ConsoleApplication\bin\Debug\netcoreapp3.0\publish\*.* publish
